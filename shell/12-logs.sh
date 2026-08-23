e117cf271355eeca99955e0dae66783f53276cdf
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 12:40:46 +0530

    shell

--- shell/12-logs.sh
+++ shell/12-logs.sh
@@ -9,7 +9,16 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-echo "logs location is:: $LOG_FILE"
+# echo "logs location is:: $LOG_FILE"
 
 mkdir -p $LOGS_FOLDER
 echo "script started executing at:: $(date)" | tee -a $LOG_FILE
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run with root user $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G You are now super user $N" | tee -a $LOG_FILE
+fi 
