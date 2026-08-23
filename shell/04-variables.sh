2830e14ac955bd86f5384cf109880dabaee6fedb
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 10:29:11 +0530

    shell

---
+++ shell/04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hey $PERSON2 Hi and How are You..?"
+echo "$PERSON2:: Hi $PERSON1 doing good whats up..?"
+echo "$PERSON1:: Nothing $PERSON2 , can we meet"
+echo "$PERSON2:: hey im going to mars can you join along with me"
+echo "$PERSON1:: no thanks, carry on once you return will meet"
