516b91d0c2bb614ff6b9e2c02fb7d693d18fbc7e
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 12:11:57 +0530

    shell

--- shell/11-colours.sh
+++ shell/11-colours.sh
@@ -12,3 +12,22 @@ then
 else
     echo -e "$G You are now super user $N"
 fi 
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 installation:: $G SUCCESS $N"
+    else
+        echo -e "$2 installation:: $R FAILURE $N"
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else 
+    echo -e "$G MYSQL already installed.. NOTHING TODO $N"
+fi
