24556eff8a8e7e92cd5fa24d779b3bafb1c8079f
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 11:32:09 +0530

    shell

--- shell/11-colours.sh
+++ shell/11-colours.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR $N:: please run with root user"
+    echo -e "$R ERROR :: please run with root user $N"
     exit 1
 else
     echo "$G You are now super user $N"
