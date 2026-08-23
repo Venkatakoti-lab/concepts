158ad4c3cd9dbb2d49f9f6d79b12dacfdd05a7b4
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 11:19:37 +0530

    shell

--- shell/10-install.sh
+++ shell/10-install.sh
@@ -10,19 +10,32 @@ else
     exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 installation:: SUCCESS"
+    else
+        echo "$2 installation:: FAILURE"
+        exit 1
+    fi
+}
 
 dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
     dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "MySql installation:: SUCCESS"
-    else
-        echo "MySql installation:: FAILURE"
-        exit 1
-    fi
+    VALIDATE $? "MYSQL"
 else
     echo "MySql already installed:: Nothing to do"
 fi 
+
+dnf list installed httpd 
+
+if [ $? -ne 0 ]
+then
+    dnf install httpd -y 
+    VALIDATE $? "HTTPD"
+else
+    echo "HTTPD already installed:: Nothing to do"
+fi
