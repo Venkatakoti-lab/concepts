a3529cf08ba51be9277491e0a36eec3a66bfea2f
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 10:24:26 +0530

    shell

---
+++ shell/02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "TRUMP:: Hey musk Hi and How are You..?"
+echo "MUSK:: Hi musk doing good whats up..?"
+echo "TRUMP:: Nothing Musk , can we meet"
+echo "MUSK:: hey im going to mars can you join along with me"
+echo "TRUMP:: no thanks, carry on once you return will meet"
