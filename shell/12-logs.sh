ce3dbfe22ebb1a74f3e4b5d291178367644d153d
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 12:36:48 +0530

    shell

--- shell/12-logs.sh
+++ shell/12-logs.sh
@@ -9,5 +9,7 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
+echo "logs location is:: $LOG_FILE"
+
 mkdir -p $LOGS_FOLDER
 echo "script started executing at:: $(date)" | tee -a $LOG_FILE
