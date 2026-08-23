5a9396206cdeb9dc3aa36f788ce2cd841946d48f
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-08-23 10:27:22 +0530

    shell

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=TRUMP
+PERSON2=MUSK
+echo "$PERSON1:: Hey $PERSON2 Hi and How are You..?"
+echo "$PERSON2:: Hi $PERSON1 doing good whats up..?"
+echo "$PERSON1:: Nothing $PERSON2 , can we meet"
+echo "$PERSON2:: hey im going to mars can you join along with me"
+echo "$PERSON1:: no thanks, carry on once you return will meet"
